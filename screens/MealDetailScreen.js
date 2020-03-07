a16d2fc5ee12082c14a5e609170c722c87ae1abd
kangchihkuo <kangchihkuo@gmail.com>
2020-03-07 22:56:07 +0800

    Passing data to the meal detail screen.

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
-
+import { MEALS } from '../data/dummy-data'
 const MealDetailScreen = props => {
+    const mealId = props.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
     return (
         <View style={styles.screen}>
-            <Text>The Meal Detail Screen!</Text>
-            <Button title="Go Back to Categories" onPress={() => {
+            <Text>{selectedMeal.title}</Text>
+            <Button title="Go Back to Categories" 
+            onPress={() => {
                 props.navigation.popToTop();
             }} />
         </View>
     );
+};
+
+MealDetailScreen.navigationOptions = navigationData => {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
+    return {
+        headerTitle: selectedMeal.title
+    };
 };
 
 const styles = StyleSheet.create({
@@ -19,7 +31,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-
 });
 
 export default MealDetailScreen;
