00c9e6ccf72a8424a7297f1fdb0ae9496ebc2ca8
kangchihkuo <kangchihkuo@gmail.com>
2020-03-07 19:24:28 +0800

    Rendering a meals list.

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 const CategoryMealsScreen = props => {
     const renderMealItem = itemData => {
-        return (<View><Text>{itemData.item.title}</Text></View>);
+        return (<MealItem
+            title={itemData.item.title}
+            image={itemData.item.imageUrl}
+            duration={itemData.item.duration}
+            complexity={itemData.item.complexity}
+            affordability={itemData.item.affordability}
+            onSelectMeal={() => { }}
+        />
+        );
     };
     const catId = props.navigation.getParam('categoryId');
     // const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
@@ -15,10 +24,11 @@ const CategoryMealsScreen = props => {
 
     return (
         <View style={styles.screen}>
-            <FlatList 
-            data={displayedMeals} 
-            keyExtractor={(item, index)=> item.id} 
-            renderItem={renderMealItem} />
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem}
+                style={{ width: '100%' }} />
             {/* <Text>The Category Meal Screen!</Text>
             <Text>{selectedCategory.title}</Text>
             <Button title="Go to Details" onPress={() => {
@@ -48,7 +58,8 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        padding: 15
     },
 });
 
