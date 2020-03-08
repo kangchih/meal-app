307dadca8b557f27850e73427fd5c1a8ee2ae066
kangchihkuo <kangchihkuo@gmail.com>
2020-03-08 22:48:22 +0800

    1. Passing  data between component and navigation options (Header) 2. Use useEffect and useCallBack to implement save button.

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
@@ -19,33 +19,55 @@ const FilterSwitch = props => {
 }
 
 const FiltersScreen = props => {
+
+    const { navigation } = props;
+
     const [isGlutenFree, setIsGlutenFree] = useState(false);
     const [isLactoseFree, setIsLactoseFree] = useState(false);
     const [isVegan, setIsVegan] = useState(false);
     const [isVegetarian, setIsVegetarian] = useState(false);
 
+    const saveFilters = useCallback(() => {
+        const appliedFilters = {
+            glutenFree: isGlutenFree,
+            lactoseFree: isLactoseFree,
+            vegan: isVegan,
+            isVegetarian: isVegetarian
+        };
+        console.log(appliedFilters);
+    }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+
+    // Use this variable which holds a pointer at this function in the end
+    // and forward this value, so forward this pointer or store this pointer to that funtion
+    // in that save key
+    useEffect(() => {
+        props.navigation.setParams({ save: saveFilters });
+        // }, [saveFilters, navigation]);  
+        //NOTE: add navigation here will cause break. Because it causes navigation to change. (infinite loop)
+    }, [saveFilters]);
+
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Available Filters / Restrictions</Text>
-            <FilterSwitch 
-            label='Gluten-free' 
-            state={isGlutenFree} 
-            onChange={newValue => setIsGlutenFree(newValue)}
+            <FilterSwitch
+                label='Gluten-free'
+                state={isGlutenFree}
+                onChange={newValue => setIsGlutenFree(newValue)}
             />
-            <FilterSwitch 
-            label='Lactose-free' 
-            state={isLactoseFree} 
-            onChange={newValue => setIsLactoseFree(newValue)}
+            <FilterSwitch
+                label='Lactose-free'
+                state={isLactoseFree}
+                onChange={newValue => setIsLactoseFree(newValue)}
             />
-            <FilterSwitch 
-            label='Vegan' 
-            state={isVegan} 
-            onChange={newValue => setIsVegan(newValue)}
+            <FilterSwitch
+                label='Vegan'
+                state={isVegan}
+                onChange={newValue => setIsVegan(newValue)}
             />
-            <FilterSwitch 
-            label='Vegetarian' 
-            state={isVegetarian} 
-            onChange={newValue => setIsVegetarian(newValue)}
+            <FilterSwitch
+                label='Vegetarian'
+                state={isVegetarian}
+                onChange={newValue => setIsVegetarian(newValue)}
             />
         </View>
     );
@@ -59,15 +81,31 @@ const FiltersScreen = props => {
 FiltersScreen.navigationOptions = (navData) => {
     return {
         headerTitle: 'Filter Meal',
-        headerLeft: () =>
+        headerLeft: () => (
             <HeaderButtons HeaderButtonComponent={HeaderButton}>
                 <Item
                     title="Menu"
                     iconName="ios-menu"
                     onPress={() => {
                         navData.navigation.toggleDrawer();
-                    }} />
+                    }}
+                />
             </HeaderButtons>
+        ),
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Save"
+                    iconName="ios-save"
+                    onPress={
+                        navData.navigation.getParam('save')}
+                // console.log('Saving filters!');
+                // navData.navigation.toggleDrawer();
+                />
+            </HeaderButtons>
+        ),
+
+
     }
 };
 
