7c185e7d03aea3f49f3a5ebff32f1b175626b586
kangchihkuo <kangchihkuo@gmail.com>
2020-03-08 17:34:01 +0800

    Configuring the drawer: Selected category to color orange.

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -87,14 +87,32 @@ const MealsFavTabNavigator =
             }
         });
 
-const FiltersNavigator = createStackNavigator({
-    Filters: FiltersScreen
-});
+const FiltersNavigator = createStackNavigator(
+    {
+        Filters: FiltersScreen
+    },
+    {
+        // mode: 'modal',
+        // navigationOptions: {
+        //     drawerLabel: 'Filters!!!'
+        // },
+        defaultNavigationOptions: defaultStackNavOptions
+    }
+);
 
 const MainNavigator = createDrawerNavigator({
-    MealsFavs: MealsFavTabNavigator,
+    MealsFavs: {
+        screen: MealsFavTabNavigator, navigationOptions: {
+            drawerLabel: 'Meals'
+        }
+    },
     Filters: FiltersNavigator
+}, {
+    contentOptions: {
+        activeTintColor: Colors.accentColor,
+        labelStyle: {
+            fontFamily: 'open-sans-bold'
+        }
+    }
 });
-
-
 export default createAppContainer(MainNavigator);
