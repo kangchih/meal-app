84ae46e13895dfc54acd9e3080f37d3e5fcefc12
kangchihkuo <kangchihkuo@gmail.com>
2020-03-09 18:31:24 +0800

    1. Adding filtering logic. 2. Dispatching filter actions.

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { CATEGORIES } from '../data/dummy-data'
 import MealList from '../components/MealList'
+import { View, StyleSheet } from 'react-native';
+import DefaultText from '../components/DefaultText'
 
 const CategoryMealsScreen = props => {
     // const renderMealItem = itemData => {
@@ -30,6 +32,14 @@ const CategoryMealsScreen = props => {
     const displayedMeals = availableMeals.filter(
         meal => meal.categoryIds.indexOf(catId) >= 0
     );
+
+    if (displayedMeals.length === 0) {
+        return (
+            <View style={styles.content}>
+                <DefaultText>No meals found, maybe check your filters?</DefaultText>
+            </View>
+        );
+    }
 
     return (
         <MealList listData={displayedMeals} navigation={props.navigation} />
@@ -66,13 +76,12 @@ CategoryMealsScreen.navigationOptions = navigationData => {
 
 };
 
-// const styles = StyleSheet.create({
-//     screen: {
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         padding: 15
-//     },
-// });
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default CategoryMealsScreen;
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -3,7 +3,8 @@ import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import Colors from '../constants/Colors';
-
+import { useDispatch } from 'react-redux';
+import { setFilters } from '../store/actions/meals';
 const FilterSwitch = props => {
     return (
         <View style={styles.filterContainer}>
@@ -27,6 +28,8 @@ const FiltersScreen = props => {
     const [isVegan, setIsVegan] = useState(false);
     const [isVegetarian, setIsVegetarian] = useState(false);
 
+    const dispatch = useDispatch();
+
     const saveFilters = useCallback(() => {
         const appliedFilters = {
             glutenFree: isGlutenFree,
@@ -34,8 +37,9 @@ const FiltersScreen = props => {
             vegan: isVegan,
             isVegetarian: isVegetarian
         };
-        console.log(appliedFilters);
-    }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+        // console.log(appliedFilters);
+        dispatch(setFilters(appliedFilters));
+    }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian, dispatch]);
 
     // Use this variable which holds a pointer at this function in the end
     // and forward this value, so forward this pointer or store this pointer to that funtion
